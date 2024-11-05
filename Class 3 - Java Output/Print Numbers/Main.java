35be009844226e95f285fab6e7c5ba85521a7393
Paulo P <ct67ca@bosch.com>
2024-11-05 10:09:05 -0300

    Dia 05/11/24-dia 1

---
+++ Class 3 - Java Output/Print Numbers/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //You can println numbers
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+
+        //You can also perform mathematical calculations inside the println
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
+    }
+}
---
+++ Class 3 - Java Output/Print Text/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); //Text must be wrapped inside double quotations marks "".
+        System.out.println("I am learning Java."); //Text must be wrapped inside double quotations marks "".
+        System.out.println("It is awesome!"); //Text must be wrapped inside double quotations marks "".
+    }
+}
