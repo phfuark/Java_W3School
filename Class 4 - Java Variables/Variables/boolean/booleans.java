488060287460a4103dcc0920c84e4bafaab8d61a
Paulo P <ct67ca@bosch.com>
2024-11-05 10:40:14 -0300

    05/11/2024-dia 1

---
+++ Class 4 - Java Variables/Variables/boolean/booleans.java
@@ -0,0 +1,8 @@
+public class booleans {
+    public static void main(String[] args) {
+
+        //type variableName = value;
+        boolean myBool = true;
+        System.out.println(myBool);
+    }
+}
---
+++ Class 4 - Java Variables/Variables/int/ints.java
@@ -0,0 +1,8 @@
+public class ints {
+    public static void main(String[] args) {
+
+        //type variableName = value;
+        int myNum = 15;
+        System.out.println(myNum);
+    }
+}
